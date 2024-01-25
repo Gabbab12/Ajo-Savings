f77544f83f73150daa658699fe42e39667d2bb5c
Ojo Gabriel <Babatundeg07@gmail.com>
2024-01-26 00:49:57 +0100

    first commit

---
+++ src/main/java/com/ajosavings/ajosavigs/AjoSavigsApplication.java
@@ -0,0 +1,13 @@
+package com.ajosavings.ajosavigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjoSavigsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjoSavigsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajosavings/ajosavigs/AjoSavigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajosavings.ajosavigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjoSavigsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
